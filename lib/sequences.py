2b67b951b904a7ed01da3b1757e8d9c3e55d8fce
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-10-24 14:02:04 -0700

    Passed all tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    count = 0
+    final = [0,1]
+    temp = 0
+    a = 0
+    b = 1
+    if length == a:
+        print([])
+    elif length == b:
+        print([0])
+    else:
+        while not(count == length-2):
+            temp = b + a
+            a = b
+            b = temp
+            final.append(b)
+            count = count + 1
+        print(final)
+    # return final
+print(print_fibonacci(10))
